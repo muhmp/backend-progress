6709b5b4cd372f250fa39f8069d9a4a27d91bcf0
muhmp <defcoolz@gmail.com>
2021-05-27 19:55:30 +0900

    init

--- dataType.php
+++ dataType.php
@@ -4,9 +4,11 @@ $s = "Hello World"; //String
 $it = 10; // integer
 $fl = 14,5;//float
 $bl = true; //boolean (true or false)
-$array = ['one','two','three']//array
+$array = ['one','two','three'];//array
 $array1 = [
     1,2,3
 ];//array
 
+//checking variable inside
+echo var_dump($it);
 ?>
